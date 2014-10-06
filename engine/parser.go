318243f09ce77639e57be8510d52567b825df9e3
Bruno Belarte <bruno.belarte@gmail.com>
2014-10-06 17:00:01 +0200

    completing readXXX functions in parser.go

--- engine/parser.go
+++ engine/parser.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"errors"
 	"regexp"
 	"strings"
 )
@@ -9,15 +10,30 @@ var (
 	tokens         []string
 	currentIndex   int
 	lastIdentifier string
+	andOperator    string
+	orOperator     string
 )
 
-func Parse(input string) Function {
+func Parse(input string) (Function, error) {
 	tokens = make([]string, 0)
 	tokens = strings.Fields(input)
 
 	currentIndex = 0
+	lastIdentifier = ""
+	andOperator = "min"
+	orOperator = "max"
 
-	return nil
+	if len(tokens) == 0 {
+		return nil, errors.New("Empty input")
+	}
+
+	result, err := readExpression()
+
+	if currentIndex != len(tokens) {
+		return nil, errors.New("Malformed expression, currentIndex!=len(tokens)")
+	}
+
+	return result, err
 }
 
 func readKeyword(s string) bool {
@@ -40,18 +56,51 @@ func readIdentifier() bool {
 	return false
 }
 
-func readExpression() Function {
-	return nil
+func readExpression() (Function, error) {
+	result, err := readAndExpression()
+
+	for readKeyword("or") && err != nil {
+		right, err2 := readAndExpression()
+		result = BinaryExpressionBuilder(result, right, NewOperator(orOperator))
+		err = err2
+	}
+
+	return result, err
 }
 
-func readAndExpression() Function {
-	return nil
+func readAndExpression() (Function, error) {
+	result, err := readNegationExpression()
+
+	for readKeyword("and") && err == nil {
+		right, err2 := readNegationExpression()
+		result = BinaryExpressionBuilder(result, right, NewOperator(andOperator))
+		err = err2
+	}
+
+	return result, err
 }
 
-func readNegationExpression() Function {
-	return nil
+func readNegationExpression() (Function, error) {
+	if readKeyword("not") {
+		result, err := readNegationExpression()
+		return NegationExpressionBuilder(result), err
+	} else {
+		return readValueExpression()
+	}
 }
 
-func readValueExpression() Function {
-	return nil
+func readValueExpression() (Function, error) {
+	switch {
+	case readKeyword("("):
+		result, err := readExpression()
+		if !readKeyword(")") {
+			return nil, errors.New("Missing closing parenthesis")
+		}
+		return result, err
+	case readIdentifier():
+		result := ValueExpressionBuilder(nil)
+		return result, nil
+	}
+
+	return nil, errors.New("An argument is missing")
 }
