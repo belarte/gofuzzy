fb25951e8100ace7a7af52f93e2408893706362b
Bruno Belarte <bruno.belarte@gmail.com>
2015-03-10 15:08:53 +0100

    engine testing: Fuzzify

---
+++ engine/engine_test.go
@@ -0,0 +1,70 @@
+package engine
+
+import (
+	"testing"
+)
+
+var (
+	testObject Object = NewObject(map[string]float64{"speed": 105, "distance": 45})
+)
+
+func SetUp() {
+	Init()
+
+	if fun, err := NewMembershipFunction("speed", "trapezoidal", []float64{0, 0, 70, 90}); err == nil {
+		knowledgeBase.AddFunction("slow", fun)
+	}
+	if fun, err := NewMembershipFunction("speed", "trapezoidal", []float64{70, 90, 90, 110}); err == nil {
+		knowledgeBase.AddFunction("good", fun)
+	}
+	if fun, err := NewMembershipFunction("speed", "trapezoidal", []float64{90, 110, 130, 130}); err == nil {
+		knowledgeBase.AddFunction("fast", fun)
+	}
+	if fun, err := NewMembershipFunction("distance", "trapezoidal", []float64{0, 0, 30, 50}); err == nil {
+		knowledgeBase.AddFunction("close", fun)
+	}
+	if fun, err := NewMembershipFunction("distance", "trapezoidal", []float64{30, 50, 100, 100}); err == nil {
+		knowledgeBase.AddFunction("far", fun)
+	}
+	if fun, err := NewMembershipFunction("response", "trapezoidal", []float64{-90, -90, -90, 0}); err == nil {
+		knowledgeBase.AddFunction("decelerate", fun)
+	}
+	if fun, err := NewMembershipFunction("response", "trapezoidal", []float64{-90, 0, 0, 90}); err == nil {
+		knowledgeBase.AddFunction("keep", fun)
+	}
+	if fun, err := NewMembershipFunction("response", "trapezoidal", []float64{0, 90, 90, 90}); err == nil {
+		knowledgeBase.AddFunction("accelerate", fun)
+	}
+	}
+
+func TestCompute(t *testing.T) {
+	t.Error("TODO")
+}
+
+func TestFuzzify(t *testing.T) {
+	SetUp()
+
+	engine.Fuzzify(testObject)
+
+	functionsName := [...]string{"slow", "good", "fast", "close", "far"}
+	expectedOutput := [...]float64{0, 0.25, 0.75, 0.25, 0.75}
+
+	for i, name := range functionsName {
+		output, check := engine.FunctionOutput(name)
+		expected := expectedOutput[i]
+		if !check {
+			t.Error("Error accessing function `", name, "'")
+		}
+		if output != expected {
+			t.Errorf("Expected: %v, got: %v", expected, output)
+		}
+	}
+}
+
+func TestInfer(t *testing.T) {
+	t.Error("TODO")
+}
+
+func TestDefuzzify(t *testing.T) {
+	t.Error("TODO")
+}
